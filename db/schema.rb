dd346c96097e1e79a1580ac32448f3cde217fa40
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-04 04:53:38 +0000

    users編集中

--- db/schema.rb
+++ db/schema.rb
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(version: 2024_04_03_114312) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.text "introduction", null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
     t.datetime "created_at", precision: 6, null: false
