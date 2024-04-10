beb5a3c2465a4c4e2802016a8eeee8bea884a166
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-10 15:38:06 +0000

    top画像の修正が必要

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,29 @@ ActiveStorage.start()
 
 // cocoon使用のため記載
 require("@nathanvda/cocoon")
+
+
+// top画面
+// blurTriggerにblurというクラス名を付ける定義
+function BlurTextAnimeControl() {
+  $('.blurTrigger').each(function(){ //blurTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('blur');// 画面内に入ったらblurというクラス名を追記
+    }else{
+    $(this).removeClass('blur');// 画面外に出たらblurというクラス名を外す
+    }
+    });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  BlurTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  BlurTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
