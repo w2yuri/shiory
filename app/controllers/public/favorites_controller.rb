f76d94d13471e82cb1ffef8568066fe7ea83f7c9
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-14 06:36:04 +0000

    いいね機能エラー有り、top修正中

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,17 @@
 class Public::FavoritesController < ApplicationController
   
   def create
-    post = Post.find(params[:post_id])
+    post = Post.find_by(id: params[:post_id])
     # カスタマーが特定の投稿をお気に入りに追加するためのFavoriteオブジェクトを作成
-    @favorite = current_customer.favorites.new(post_id: post.id)
+    @favorite = current_customer.favorites.new(id: params[:post_id])
     @favorite.save
     render 'replace_btn'
   end
 
   def destroy
-    post = Post.find(params[:post_id])
+    post = Post.find(id: params[:post_id])
     # カスタマーが特定の投稿をお気に入りに追加したかどうかを確認し、その結果を変数に代入
-    @favorite = current_customer.favorites.find_by(post_id: post.id)
+    @favorite = current_customer.favorites.find_by(id: params[:post_id])
     @favorite.destroy
     render 'replace_btn'
   end
