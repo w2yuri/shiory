cc43dabfd74443f9491d159ae0026f691c66e2de
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-15 04:59:55 +0000

    テストチェック完了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,11 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name, presence: { message: "を入力してください" }
+
   has_many :post_images, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
