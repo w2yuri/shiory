4aba1ab96cb928cc145959bca16d8f6b6b1cdfe5
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-15 07:04:40 +0000

    adminページ追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,31 @@
 class Admin::CustomersController < ApplicationController
+  # seedが読み込まれログインできるようになったらコメントアウト解除
+  # before_action :authenticate_admin!
+  
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admon_customer_path(@customer), notice: "顧客情報が更新されました。"
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :email, :is_active)
   end
 end
