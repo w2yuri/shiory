81038274683b296d928c68d4b05bb0cf3aaefede
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-15 10:59:44 +0000

    post/show修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -48,19 +48,19 @@ class Public::CustomersController < ApplicationController
   end
   
   # 検索機能
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @customer = Customer.where("name LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @customer = Customer.where("name LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @customer = Customer.where("name LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @customer = Customer.where("name LIKE?","%#{word}%")
-    else
-      @customer = Customer.all
-    end
-  end
+  # def self.looks(search, word)
+  #   if search == "perfect_match"
+  #     @customer = Customer.where("name LIKE?", "#{word}")
+  #   elsif search == "forward_match"
+  #     @customer = Customer.where("name LIKE?","#{word}%")
+  #   elsif search == "backward_match"
+  #     @customer = Customer.where("name LIKE?","%#{word}")
+  #   elsif search == "partial_match"
+  #     @customer = Customer.where("name LIKE?","%#{word}%")
+  #   else
+  #     @customer = Customer.all
+  #   end
+  # end
   
    def self.looks(search, word)
    end
