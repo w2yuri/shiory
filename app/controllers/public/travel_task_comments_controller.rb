b0a45d03be6bbb0be6b98cd712260b8d1ce5bd53
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-21 04:02:12 +0000

    [エラー修正：メモ追加]コントローラ等にコメントアウト

--- app/controllers/public/travel_task_comments_controller.rb
+++ app/controllers/public/travel_task_comments_controller.rb
@@ -1,21 +1,22 @@
 class Public::TravelTaskCommentsController < ApplicationController
 
   def create
-    # @travel_task = TravelTask.find(comment_params[:travel_task_id])
-    # @comment = TravelTaskComment.new(comment_params)
-    # @comment.customer_id = current_customer.id
-    # @comment.travel_task_id = @travel_task.id
-    # @comment.save
+    # params[:post_id]を使用し、コメントを追加する対象の投稿（Post）を特定。
+    # その投稿に紐づくTravelTaskを、comment_params[:travel_task_id]を使用して特定。
+    # 新しいコメントを作成するため、特定された@travel_taskに対して、travel_task_comments.new(comment_params)を呼び出す。
     @travel_task = Post.find(params[:post_id]).travel_tasks.find(comment_params[:travel_task_id])
     @travel_task.travel_task_comments.new(comment_params).save
-    render 'create'
+    render 'action'
   end
 
   def destroy
-    # @travel_task = TravelTask.find(params[:post_id])
+    # params[:post_id]を使用し、コメントが属する投稿（Post）を特定。
+    # その投稿に紐づくTravelTaskを、params[:travel_task_id]を使用して特定。
+    # その旅行タスクに紐づくTravelTaskCommentを、params[:id]を使用し特定。
+    # 特定されたコメントをデータベースから削除。
     @travel_task = Post.find(params[:post_id]).travel_tasks.find(params[:travel_task_id])
     @travel_task.travel_task_comments.find(params[:id]).destroy
-    render 'create'
+    render 'action'
   end
 
   private
