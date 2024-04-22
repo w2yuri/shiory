7bb6b22a82dbcbd52456631b0f3af9ce6881e765
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-22 05:29:15 +0000

    [エラー:有]DMにエラー有り

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -1,61 +1,37 @@
 class Public::ChatsController < ApplicationController
   # フォロー関係がない場合に、投稿一覧ページにリダイレクト
-  # before_action :reject_non_related, only: [:show]
+  before_action :reject_non_related, only: [:show]
 
-  def show
-    # #チャットする相手を特定
-    # @customer = Customer.find(params[:id])
-    # #ログイン中のユーザーの部屋情報を全て取得
-    # rooms = current_customer.customer_chat_rooms.pluck(:chat_room_id)
-    # #その中にチャットする相手とのルームがあるか確認
-    # customer_chat_rooms = CustomerChatRoom.find_by(customer_id: @customer.id, chat_room_id: rooms)
-
-    # #ユーザールームがある場合
-    # unless customer_chat_rooms.nil?
-    #   #変数@roomにユーザー（自分と相手）と紐づいているroomを代入
-    #   @room = customer_chat_rooms
-    # else
-    #   #ユーザールームが無かった場合、新しくRoomを作成
-    #   @room = ChatRoom.new
-    #   @room.save
-    #   #自分の中間テーブルを作成
-    #   CustomerChatRoom.create(customer_id: current_customer.id, chat_room_id: @room.id)
-    #   #相手の中間テーブルを作成
-    #   CustomerChatRoom.create(customer_id: @customer.id, chat_room_id: @room.id)
-    # end
-    # #チャットの一覧
-    # # @chats = @room.chats
-    # @chats = @room.chat_room.chats
-    # #チャットの投稿
-    # @chat = Chat.new(chat_room_id: @room.chat_room.id)
-
+    # データベースから対象のカスタマーを検索
     @target_customer = Customer.find(params[:id])
+    # ログインしているカスタマーのチャットルームのIDを取得
     my_rooms = current_customer.customer_chat_rooms.pluck(:chat_room_id)
+    # 対象のカスタマーとログインしているカスタマーのチャットルームを検索
     target_room = CustomerChatRoom.find_by(customer_id: @target_customer, chat_room_id: my_rooms)
 
+    # チャットルームが存在しているか確認
     unless target_room.nil?
       @room = target_room.chat_room
     else
-      @room = ChatRoom.create()
+      @room = ChatRoom.create
+      # ログインしているカスタマーとチャットルームの中間テーブル作成
       CustomerChatRoom.create(customer_id: current_customer.id, chat_room_id: @room.id)
+      # 対象のカスタマーとチャットルームの中間テーブル作成
       CustomerChatRoom.create(customer_id: @target_customer.id, chat_room_id: @room.id)
     end
-
-
-    @chats = @room.chats
-    @chat = Chat.new(chat_room_id: @room.id)
-
-  end
+      @chats = @room.chats
+      @chat = Chat.new(chat_room_id: @room.id)
+  　end
 
   def create
     @chat = current_customer.chats.new(chat_params)
+    # idが一致するチャットルーム取得
     @room = ChatRoom.find_by(id: params[:chat][:chat_room_id])
+    # @roomが存在する場合のみ、チャット一覧を取得
     @chats = @room.chats if @room
-    pp chat_params
+    # チャットの保存に失敗した場合、jsのエラーメッセージを表示し、チャットの保存にしたら続行
     render :validate, formats: :js unless @chat.save
   end
-
-
 
   private
 
@@ -64,10 +40,10 @@ class Public::ChatsController < ApplicationController
   end
 
   # フォロー関係がない場合に、投稿一覧ページにリダイレクト
-  # def reject_non_related
-  #   customer = Customer.find(params[:id])
-  #   unless current_customer.following?(customer) && customer.following?(current_customer)
-  #     redirect_to posts_path
-  #   end
-  # end
+  def reject_non_related
+    customer = Customer.find(params[:id])
+    unless current_customer.following?(customer) && customer.following?(current_customer)
+      redirect_to posts_path
+    end
+  end
 end
