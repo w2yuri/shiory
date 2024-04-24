07bf67953f83d114d39fd44968faeb00b4c6701d
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-24 04:56:45 +0000

    [追加]投稿日時をpostにのみ追加

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Shiory
     config.load_defaults 6.1
     # 日本語化
     config.i18n.default_locale = :ja
-
+    config.time_zone = 'Tokyo'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
