63014fe452164eda3f7575aa5fb6a3d0aa5eac18
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-24 11:36:16 +0000

    [仮]era-notame

--- app/controllers/admin/chats_controller.rb
+++ app/controllers/admin/chats_controller.rb
@@ -1,31 +1,59 @@
 class Admin::ChatsController < ApplicationController
+  before_action :find_target_customer, only: [:show]
 
-    # データベースから対象のカスタマーを検索
-    @target_customer = Customer.find(params[:id])
-    # ログインしているカスタマーのチャットルームのIDを取得
+  def show
     my_rooms = current_customer.customer_chat_rooms.pluck(:chat_room_id)
-    # 対象のカスタマーとログインしているカスタマーのチャットルームを検索
-    target_room = CustomerChatRoom.find_by(customer_id: @target_customer, chat_room_id: my_rooms)
+    target_room = CustomerChatRoom.find_by(customer_id: @target_customer.id, chat_room_id: my_rooms)
 
-    # チャットルームが存在しているか確認
-    unless target_room.nil?
+    if target_room
       @room = target_room.chat_room
     else
       @room = ChatRoom.create
-      # ログインしているカスタマーとチャットルームの中間テーブル作成
       CustomerChatRoom.create(customer_id: current_customer.id, chat_room_id: @room.id)
-      # 対象のカスタマーとチャットルームの中間テーブル作成
       CustomerChatRoom.create(customer_id: @target_customer.id, chat_room_id: @room.id)
     end
-      @chats = @room.chats
-      @chat = Chat.new(chat_room_id: @room.id)
-  　end
 
+    @chats = @room.chats
+    @chat = Chat.new(chat_room_id: @room.id)
+  end
 
   private
+
+  def find_target_customer
+    @target_customer = Customer.find(params[:id])
+  end
 
   def chat_params
     params.require(:chat).permit(:message, :chat_room_id)
   end
-
 end
+
+#     # データベースから対象のカスタマーを検索
+#     @target_customer = Customer.find(params[:id])
+#     # ログインしているカスタマーのチャットルームのIDを取得
+#     my_rooms = current_customer.customer_chat_rooms.pluck(:chat_room_id)
+#     # 対象のカスタマーとログインしているカスタマーのチャットルームを検索
+#     target_room = CustomerChatRoom.find_by(customer_id: @target_customer, chat_room_id: my_rooms)
+
+#     # チャットルームが存在しているか確認
+#     unless target_room.nil?
+#       @room = target_room.chat_room
+#     else
+#       @room = ChatRoom.create
+#       # ログインしているカスタマーとチャットルームの中間テーブル作成
+#       CustomerChatRoom.create(customer_id: current_customer.id, chat_room_id: @room.id)
+#       # 対象のカスタマーとチャットルームの中間テーブル作成
+#       CustomerChatRoom.create(customer_id: @target_customer.id, chat_room_id: @room.id)
+#     end
+#       @chats = @room.chats
+#       @chat = Chat.new(chat_room_id: @room.id)
+#   　end
+
+
+#   private
+
+#   def chat_params
+#     params.require(:chat).permit(:message, :chat_room_id)
+#   end
+
+# end
