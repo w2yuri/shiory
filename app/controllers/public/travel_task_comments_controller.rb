228b66ed419f79e6d7866d8358720ff0092e446d
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-04-26 04:20:46 +0000

    [一部修正：エラー有]travel_taskコメントの作成OK、削除はできるが非同期になっていない、エラー文が出ない

--- app/controllers/public/travel_task_comments_controller.rb
+++ app/controllers/public/travel_task_comments_controller.rb
@@ -1,28 +1,56 @@
 class Public::TravelTaskCommentsController < ApplicationController
   before_action :authenticate_customer!
 
+#   def create
+#     # params[:post_id]を使用し、コメントを追加する対象の投稿（Post）を特定。
+#     # その投稿に紐づくTravelTaskを、comment_params[:travel_task_id]を使用して特定。
+#     # 新しいコメントを作成するため、特定された@travel_taskに対して、travel_task_comments.new(comment_params)を呼び出す。
+#     @travel_task = Post.find(params[:post_id]).travel_tasks.find(comment_params[:travel_task_id])
+#     @travel_task.travel_task_comments.new(comment_params).save
+#     # コメントの保存に失敗した場合、jsのエラーメッセージを表示し、コメントの保存にしたら続行
+#     render :validate, formats: :js unless @travel_task.comment.save
+#   end
+
+#   def destroy
+#     # params[:post_id]を使用し、コメントが属する投稿（Post）を特定。
+#     # その投稿に紐づくTravelTaskを、params[:travel_task_id]を使用して特定。
+#     # その旅行タスクに紐づくTravelTaskCommentを、params[:id]を使用し特定。
+#     # 特定されたコメントをデータベースから削除。
+#     @travel_task = Post.find(params[:post_id]).travel_tasks.find(params[:travel_task_id])
+#     @travel_task.travel_task_comments.find(params[:id]).destroy
+#     render 'create'
+#   end
+  
+#   @travel_task = Post.find(params[:post_id]).travel_tasks.find(comment_params[:travel_task_id])
+#     @travel_task.travel_task_comments.new(comment_params).save
+#     render 'create'
+
+#   private
+
+#   def comment_params
+#     params.require(:travel_task_comment).permit(:content, :travel_task_id).merge(customer_id: current_customer.id)
+#   end
+# end
+
   def create
-    # params[:post_id]を使用し、コメントを追加する対象の投稿（Post）を特定。
-    # その投稿に紐づくTravelTaskを、comment_params[:travel_task_id]を使用して特定。
-    # 新しいコメントを作成するため、特定された@travel_taskに対して、travel_task_comments.new(comment_params)を呼び出す。
-    @travel_task = Post.find(params[:post_id]).travel_tasks.find(comment_params[:travel_task_id])
-    @travel_task.travel_task_comments.new(comment_params).save
-     # チャットの保存に失敗した場合、jsのエラーメッセージを表示し、チャットの保存にしたら続行
-    render :validate, formats: :js unless @travel_task.save
-  end
-
+    @travel_task = TravelTask.find(comment_params[:travel_task_id])
+    @comment = TravelTaskComment.new(comment_params)
+    @comment.customer_id = current_customer.id
+    @comment.travel_task_id = @travel_task.id
+    @comment.save
+    render 'action'
+    
+  end 
+  
   def destroy
-    # params[:post_id]を使用し、コメントが属する投稿（Post）を特定。
-    # その投稿に紐づくTravelTaskを、params[:travel_task_id]を使用して特定。
-    # その旅行タスクに紐づくTravelTaskCommentを、params[:id]を使用し特定。
-    # 特定されたコメントをデータベースから削除。
-    @travel_task = Post.find(params[:post_id]).travel_tasks.find(params[:travel_task_id])
-    @travel_task.travel_task_comments.find(params[:id]).destroy
-  end
-
+     @travel_task = Post.find(params[:post_id]).travel_tasks.find(params[:travel_task_id])
+     @travel_task.travel_task_comments.find(params[:id]).destroy
+     render 'destroy'
+  end 
+  
   private
 
   def comment_params
-    params.require(:travel_task_comment).permit(:content, :travel_task_id).merge(customer_id: current_customer.id)
-  end
+    params.require(:travel_task_comment).permit(:content, :travel_task_id) 
+  end 
 end
