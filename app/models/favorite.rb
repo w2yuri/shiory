b31e267496de8f04912a4f375feccf159b56892d
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-05-26 14:44:24 +0000

    更新

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -7,7 +7,7 @@ class Favorite < ApplicationRecord
   after_create do
     create_notification(customer_id: post.customer_id)
   end
-  
+
   def notification_message
     "投稿した#{post.title}が#{customer.name}さんにいいねされました"
   end
@@ -15,7 +15,7 @@ class Favorite < ApplicationRecord
   def notification_path
      customer_path(customer)
   end
-  
+
   # 指定された属性の値が一意であること(いいねは１投稿につき１度のみ)
   validates_uniqueness_of :post_id, scope: :customer_id
 end
